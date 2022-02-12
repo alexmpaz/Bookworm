501d818c1501c5d04deca65cea30df19f1c66d20
Alex Paz <alexmpaz@gmail.com>
2022-02-12 12:24:34 +0000

    feat: AddBookView

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,34 +9,34 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddBook = false
     
     var body: some View {
-        VStack {
-            List(students) { student in
-                Text(student.name ?? "Unknown")
-                // when accessing the data, it returns optionals that require nil coalescing, like above
-            }
-            
-            Button("Add") {
-                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
-                
-                let chosenFirstName = firstNames.randomElement()!
-                let chosenLastName = lastNames.randomElement()!
-                
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? moc.save()
-            }
+        NavigationView {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookworm")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            showingAddBook.toggle()
+                        } label: {
+                            Label("Add Book", systemImage: "plus")
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddBook) {
+                    AddBookView()
+                }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    @Environment(\.managedObjectContext) var moc
+    
     static var previews: some View {
         ContentView()
     }
